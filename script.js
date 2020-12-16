601261158192a1689d6673e4f07757d315b6d071
Mustafa Balci <72220498+mbalci85@users.noreply.github.com>
2020-12-16 15:17:30 +0000

    bmi

--- script.js
+++ script.js
@@ -1,11 +1,36 @@
-let massMark = 75;
-let massJohn = 86;
-let heightMark = 1.67;
-let heightJohn = 1.77;
+// let massMark = 75;
+// let massJohn = 56;
+// let heightMark = 1.67;
+// let heightJohn = 1.77;
+
+let massMark = prompt("Enter Mark's weight in kg: ");
+let massJohn = prompt("Enter John's weight in kg: ");
+let heightMark = prompt("Enter Mark's height in m: ");
+let heightJohn = prompt("Enter John's height in m: ");
 
 //BMIs of Mark and John
-let BMIMark = massMark / (heightMark * heightMark);
-let BMIJohn = massJohn / (heightJohn * heightJohn);
+let BMIMark = Math.round(massMark / (heightMark * heightMark));
+let BMIJohn = Math.round(massJohn / (heightJohn * heightJohn));
 
-console.log(`Mark's BMI: ${BMIMark}
+alert(`Mark's BMI: ${BMIMark}
 John's BMI: ${BMIJohn}`);
+
+// let higherBMI = BMIJohn > BMIMark;
+
+// console.log(
+// 	`Is Mark's BMI higher than John's? Answer is 'false'. Because Mark's BMI is ${BMIMark} and John's BMI is ${BMIJohn}`,
+// );
+
+if (BMIMark > BMIJohn) {
+	alert(
+		`Is Mark's BMI higher than John's? Answer is 'true'. Because Mark's BMI is ${BMIMark} and John's BMI is ${BMIJohn}`,
+	);
+} else if (BMIMark < BMIJohn) {
+	alert(
+		`Is Mark's BMI higher than John's? Answer is 'false'. Because Mark's BMI is ${BMIMark} and John's BMI is ${BMIJohn}`,
+	);
+} else {
+	alert(
+		`Mark's BMI and John's BMI are equal since their BMIs are both ${BMIJohn}`,
+	);
+}
