4074e7203ddb451842484701ed00ef492de1d299
Mustafa Balci <72220498+mbalci85@users.noreply.github.com>
2020-12-16 12:58:41 +0000

    calculate and compare BMIs

---
+++ script.js
@@ -0,0 +1,11 @@
+let massMark = 75;
+let massJohn = 86;
+let heightMark = 1.67;
+let heightJohn = 1.77;
+
+//BMIs of Mark and John
+let BMIMark = massMark / (heightMark * heightMark);
+let BMIJohn = massJohn / (heightJohn * heightJohn);
+
+console.log(`Mark's BMI: ${BMIMark}
+John's BMI: ${BMIJohn}`);
